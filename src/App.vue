71f208e295d7795caad6ec67ce7c6ffff1a50aad
CGibaux <costin.gibaux@ynov.com>
2019-11-09 18:07:53 +0100

    finish the search bar

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-app-bar
+            app
+            color="#4286f4"
+            dark
+    >
+
+      <div class="d-flex align-center">
+        <v-container>
+          <v-btn href="http://localhost:8080/">
+            <v-toolbar-title class="headline text-uppercase">
+              <span> Home &nbsp;&nbsp;</span>
+            </v-toolbar-title>
+          </v-btn>
+        </v-container>
+      </div>
+      <div>
+        <v-container>
+          <v-btn href="http://localhost:8080/api">
+            <v-toolbar-title class="headline text-uppercase">
+              <span class="font-weight-bold"> API de bière</span>
+            </v-toolbar-title>
+          </v-btn>
+        </v-container>
+      </div>
+
+
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<!--
+
+
+
+-->
---
+++ src/views/Api.vue
@@ -0,0 +1,163 @@
+<template>
+    <v-app dark class="back">
+        <v-content>
+        <div>
+            <div class="rechercheBarre">
+                <input type="text" v-model="search" placeholder="Chercher une bière"/>
+                <!--<v-icon style="color: black">fas fa-search</v-icon>-->
+            </div>
+            <v-container >
+                <v-layout justify-center="">
+                    <v-flex xs6>
+                        <button @click="getMyBeer" class="buttonBeer">Veux tu ma bière jeune enfant</button>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+            <div style="padding-top: 50px">
+                <div class="cadreConteneur" v-for="element in filter"
+                     :key="element.id">
+                    <span><span class="boldRed" style="font-weight: bold">Nom de la bière </span><span class="text"> : {{ element.name }} ({{element.abv}}°) :  </span></span>
+                    <div class="cadreGrid">
+                        <br>
+                        <span class="boldRed"> La description voyons : </span><span class="text">{{ element.description }}</span>
+                    </div>
+                    <div class="cadreGrid">
+                        <br>
+                        <span class="boldBlue">Première fois brassée : </span><span class="text">{{ element.first_brewed }}</span>
+                    </div>
+                    <div class="cadreGrid">
+                        <br>
+                        <img class="img" v-bind:src="element.image_url">
+                    </div>
+                </div>
+            </div>
+            <v-container>
+
+            </v-container>
+
+
+            <v-container>
+
+            </v-container>
+        </div>
+
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    /* La classe du composant API.vue */
+    export default {
+        data(){
+            return {
+                beers: [],
+                search: ''
+            }
+        },
+        methods: {
+            getMyBeer() {
+                axios.get('https://api.punkapi.com/v2/beers')
+                    .then((beer) => {
+                        console.log(beer.request.response);
+                        this.beers = JSON.parse(beer.request.response)
+                    })
+                    .catch(() => { // function()
+                            this.beers = "pas de bière";
+                        }
+                    )
+            }
+        },
+        computed: {
+            filter: function () {
+                return this.beers.filter((blog) => {
+                    return blog.name.match(this.search)
+                })
+            }
+        }
+    };
+</script>
+https://uigradients.com/#Memariani
+<style scoped>
+    .back{
+        background: linear-gradient(#203A43, #2C5364);
+    }
+    .text{
+        color: #42b983;
+    }
+    .boldGreen {
+        font-weight: bold;
+        color: #42b983;
+        text-decoration: underline;
+        font-size: larger;
+    }
+    .boldRed{
+        font-weight: bold;
+        color: crimson;
+        text-decoration: underline;
+        font-size: larger;
+    }
+    .boldBlue{
+        font-weight: bold;
+        color: blue;
+        text-decoration: underline;
+        font-size: larger;
+    }
+    .cadreConteneur{
+        display: grid;
+        grid-template-columns: auto auto auto;
+        padding: 10px;
+    }
+    .cadreGrid {
+        border: 1px solid rgba(0, 0, 0, 0.8);
+        padding: 20px;
+
+        text-align: center;
+    }
+    .img{
+        width: auto;
+        height: 300px;
+    }
+    .buttonBeer{
+        font-size: 24px;
+        font-family: "Bauhaus 93";
+        color: white;
+        background: linear-gradient(#FFC465, #FFBB4D);
+        background-image: linear-gradient(to right, #4CB8C4 0%, #3CD3AD 51%, #4CB8C4 100%);
+        transition-duration: 0.4s;
+        width: 550px;
+        height: 75px;
+
+    }
+    .buttonBeer:hover{
+        background-position: right center;
+        background-color: black;
+        border-radius: 50px;
+        box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    }
+    .buttonBeer:active{
+        background-color: #3e8e41;
+        box-shadow: 0 5px #666;
+        transform: translateY(4px);
+    }
+    .rechercheBarre{
+        position: absolute;
+        top: 1.5vw;
+        left: 140vh;
+
+    }
+    input{
+        border: 3px solid #32e17c;
+        height: 40px;
+        width: 300px;
+        border-radius: 50px;
+        padding: 0px 10px;
+        color: #203A43;
+        background-color: white;
+    }
+    input:hover{
+        transition: width 0.2s ease-out;
+        width: 500px;
+    }
+</style>
